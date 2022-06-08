913901c3efe159e2bc7f89bdbe552af3888b1938
Fredi Yonatan Florez Garzon <fflorezg@everis.com>
2022-06-08 09:34:30 -0500

    Se agrega manejo de excepciones en codigo

--- test/main.py
+++ test/main.py
@@ -7,6 +7,13 @@ import requests
 
 def run():
     r = requests.get('https://api.chucknorris.io/jokes/random')
-    with open('json-joker.json', 'w') as dataFile:
-        json.dump(r.json(), dataFile, indent=4)
-    return 1
+    
+    try:
+        with open('json-joker.json', 'w') as dataFile:
+            json.dump(r.json(), dataFile, indent=4)
+        print("Se escribe en el archivo sin problemas")
+        return 0
+    except:
+        print("Error al escribir en el archivo")
+        return 1
+    
